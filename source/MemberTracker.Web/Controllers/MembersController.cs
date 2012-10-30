2bdfd30ddf16e11e5a2a06d3458c2df721ec26a7
Justin Kohnen <jkohnen@gmail.com>
2012-10-30 16:33:07 -0400

    Adding logic to get specific member information

--- source/MemberTracker.Web/Controllers/MembersController.cs
+++ source/MemberTracker.Web/Controllers/MembersController.cs
@@ -12,32 +12,41 @@ namespace MemberTracker.Web.Controllers
 		public Guid id { get; set; }
 		public string firstName { get; set; }
 		public string lastName { get; set; }
-		public DateTime startData { get; set; }
+		public DateTime startDate { get; set; }
 		public int numDrinks { get; set; }
 	}
 
 	public class MembersController : ApiController
 	{
-
-		// GET api/values
-		public IEnumerable<Member> Get()
+		private List<Member> buildList()
 		{
-
 			var members = new List<Member>();
 
-			members.Add(new Member(){id = new Guid("52789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Justin", lastName = "Kohnen", startData = new DateTime(2012, 6, 15), numDrinks = 129});
-			members.Add(new Member(){id = new Guid("62789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Doug", lastName = "McSwiggen", startData = new DateTime(2012, 7, 15), numDrinks = 193});
-			members.Add(new Member(){id = new Guid("72789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Jane", lastName = "Doe", startData = new DateTime(2102,8,15), numDrinks = 5});
-			members.Add(new Member(){id = new Guid("82789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Santa", lastName = "Clause", startData = new DateTime(2012,9,15), numDrinks = 0});
-			members.Add(new Member(){id = new Guid("92789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Easter", lastName = "Bunny", startData = new DateTime(2012,9,15), numDrinks = 42});
+			members.Add(new Member() { id = new Guid("52789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Justin", lastName = "Kohnen", startDate = new DateTime(2012, 6, 15), numDrinks = 129 });
+			members.Add(new Member() { id = new Guid("62789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Doug", lastName = "McSwiggen", startDate = new DateTime(2012, 7, 15), numDrinks = 193 });
+			members.Add(new Member() { id = new Guid("72789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Jane", lastName = "Doe", startDate = new DateTime(2102, 8, 15), numDrinks = 5 });
+			members.Add(new Member() { id = new Guid("82789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Santa", lastName = "Clause", startDate = new DateTime(2012, 9, 15), numDrinks = 0 });
+			members.Add(new Member() { id = new Guid("92789726-3b65-43b6-a1b0-8be69ea28ffb"), firstName = "Easter", lastName = "Bunny", startDate = new DateTime(2012, 9, 15), numDrinks = 42 });
 
 			return members;
 		}
 
-		// GET api/values/5
-		public Member Get(int id)
+		// GET api/values
+		public IEnumerable<Member> Get()
 		{
-			return new Member();
+			return buildList();
+		}
+
+		// GET api/values/5
+		public Member Get(Guid id)
+		{
+			var found = buildList().FirstOrDefault(member => member.id == id);
+			if(found == null)
+			{
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+
+			return found;
 		}
 
 		// POST api/values
